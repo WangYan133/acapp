23baedc6993490bde1ea2fd23a49efe6a03ab37d
wyan <1339101035@qq.com>
2023-03-10 18:16:25 +0800

    django first small web

--- game/views.py
+++ game/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    line1 = '<h1 style = "text-align: center"> 术士之战 </h1>'
+    line2 = '<img src = "https://img.3dmgame.com/uploads/allimg/140323/235_140323205352_1_lit.jpg" width = 1800>'
+    return HttpResponse(line1 + line2)
